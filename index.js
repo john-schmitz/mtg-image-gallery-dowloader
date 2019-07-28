22fba6f33c06b2bd2905571f4fb6d5729288b75f
John Schmitz <johngc2010@hotmail.com>
2019-07-28 17:39:22 -0300

    Refactor some code and updated dependencies

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
-const file = "./res/AllSets.json";
+const throttledQueue = require("throttled-queue");
+const jsonfile = require("jsonfile");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const finalPath = "./cardImages"
+const file = "./res/AllCards.json";
+const unsets = ["UST", "UNH", "UGL"];
+
+function intersection(arr1, arr2) {
+	return arr1.some(r => arr2.includes(r));
+}
 
 async function boot() {
-	const jsonfile = require("jsonfile");
-	const listaCartas = [];
-	jsonfile.readFile(file)
-		.then((obj) => {
-			for (const key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					const set = obj[key];
-					if (set.baseSetSize === 0) {
-						continue;
-					}
-					for (let i = 0; i < set.cards.length; i++) {
-						const card = set.cards[i];
-						const nomeCarta = card.name.replace(/\([a-z]\)/, "");
-						if (!listaCartas.includes(nomeCarta)) {
-							listaCartas.push(nomeCarta);
-						}
-					}
-
-				}
-
+	const throttle = throttledQueue(10, 1000, true);
+	const obj = await jsonfile.readFile(file);
+	for (const key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			const card = obj[key];
+			if (intersection(card.printings, unsets)) {
+				continue;
 			}
-			const throttledQueue = require("throttled-queue");
-			const throttle = throttledQueue(1, 200);
-			const fetch = require("node-fetch");
-			const fs = require("fs");
-			for (let i = 0; i < listaCartas.length; i++) {
-				throttle(() => {
-					const nomeCarta = listaCartas[i].replace(/ $/, "").replace(" // ", "");
-					fs.access(`./cardImages/${nomeCarta}.full.jpg`, fs.constants.F_OK, (err) => {
-						if (err)
-							fetch("https://api.scryfall.com/cards/named?exact=" + nomeCarta).then((res) => {
-								return res.json();
-							}).then((carta) => {
-								const imageUrl = carta.image_uris.border_crop;
-								fetch(imageUrl).then(res => {
-									console.log("baixado ", carta.name);
-									const dest = fs.createWriteStream(`./cardImages/${carta.name.replace(/"/g, "").replace(" // ", "").replace(/\?/g, "")}.full.jpg`);
-									res.body.pipe(dest);
-								}).catch(err => {
-									console.log(err);
-								});
-							}).catch((err) => {
+			const nomeCarta = card.name.replace(/\([a-z]\)/, "").replace(/ $/, "").replace(" // ", "");
+			throttle(() => {
+				fs.access(`${finalPath}/${nomeCarta}.full.jpg`, fs.constants.F_OK, async (err) => {
+					if (err) {
+							let res = await fetch("https://api.scryfall.com/cards/named?exact=" + nomeCarta);
+							let cardObject = await res.json()
+							let imageUrl;
+
+							if(cardObject.image_uris) {
+								imageUrl = cardObject.image_uris.border_crop;
+							} else if (cardObject.card_faces) {
+								imageUrl = cardObject.card_faces[0].image_uris.border_crop;
+							}
+
+							fetch(imageUrl).then(res => {
+								console.log("baixado ", cardObject.name);
+								const dest = fs.createWriteStream(`${finalPath}/${cardObject.name.replace(/"/g, "").replace(" // ", "").replace(/\?/g, "")}.full.jpg`);
+								res.body.pipe(dest);
+							}).catch(err => {
 								console.log(err);
 							});
-					});
+					}
 				});
-			}
-		})
-		.catch(error => console.error(error));
+			});
+		}
+	}
 }
 boot();
